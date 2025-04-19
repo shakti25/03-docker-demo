bf9f51c3e0f962ef2b8a9fac685797d22a335dfa
Rajiv Toora <rajiv.toora@gmail.com>
2025-04-19 02:03:01 -0500

    adding Redis

--- src/DockerDemo03.Web/Program.cs
+++ src/DockerDemo03.Web/Program.cs
@@ -1,6 +1,7 @@
 using DockerDemo03.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,12 @@ builder.Host.UseSerilog();
 // Configurar la conexión a la base de datos
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// configurar Redis
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp => {
+    var configuration = builder.Configuration.GetSection("Redis")["ConnectionString"];
+    return ConnectionMultiplexer.Connect(configuration);
 });
 
 // Add services to the container.
